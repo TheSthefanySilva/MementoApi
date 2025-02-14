2fe413b0c5be18a54eb6e4c4d796cd90a06c19a3
Sthefany <sthefany.cristovam@pwi.com.br>
2025-02-14 14:21:33 -0300

    Add project files.

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,21 @@
+﻿using MementoDominio.Manipuladores;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MementoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriaController : Controller
+    {
+        // GET: CategoriaController
+        [HttpGet]
+        public ActionResult<List<string>> ListarCategorias()
+        {
+            
+            var categoriaManipulador = new CategoriaManipulador();
+            return categoriaManipulador.ListarCategorias();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//todo: melhoria, utilizar addScoped pro contexto e usar appsettings pra string
+//builder.Services.AddScoped(x => new ContextoBd());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
